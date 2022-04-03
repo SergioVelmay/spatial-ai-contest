c59e3258acca86b68b514b813dc4a82fa0bfe9c2
Sergio Velmay <sergiovelmay@gmail.com>
2022-04-03 20:40:23 +0200

    added drawings for rect and text

--- poka-yoke-assembly/poka_yoke_assembly.py
+++ poka-yoke-assembly/poka_yoke_assembly.py
@@ -6,18 +6,49 @@ class PokaYokeAssembly:
         self.Window = AssemblyWindow()
         self.Cameras = AssemblyCameras(self.DrawImages)
 
+        self.label_image_length = 400
+
         self.PrimaryImage = None
         self.ModelImage = None
         self.SecondaryImage = None
 
-    def DrawImages(self, primary_image: np.ndarray, model_image: np.ndarray, secondary_image: np.ndarray):
+    def DrawImages(self, primary_image: np.ndarray, model_image: np.ndarray, secondary_image: np.ndarray, classifications: list, detections: list):
         if primary_image.any() and model_image.any() and secondary_image.any():
 
             DrawRectangle(primary_image, (159, 160), (229, 230), COLOR_CV_BLUE)
             DrawText(primary_image, 'assembly', (159-25, 160-10), COLOR_CV_BLUE)
+            if len(classifications) == 1:
+                color = COLOR_CV_GREEN
+                label = classifications[0].Label
 
-            DrawRectangle(model_image, (5, 5), (395, 395), COLOR_CV_BLUE)
-            DrawText(model_image, 'label', (5+5, 5+25), COLOR_CV_BLUE)
+                if '0_' in label:
+                    color = COLOR_CV_BLUE
+                if '7_' in label:
+                    color = COLOR_CV_WHITE
+                if '_ko' in label:
+                    color = COLOR_CV_RED
+                
+                text = label[2:] + ' {:.2f}'.format(classifications[0].Score)
+                
+                DrawRectangle(model_image, (5, 5), (395, 395), color)
+                DrawText(model_image, text, (5+5, 5+25), color)
+
+            if len(detections) == 3:
+                for detection in detections:
+                    color = COLOR_CV_WHITE
+                    label = detection.Label
+                    
+                    if 'gear' in label:
+                        color = COLOR_CV_GREEN
+                    
+                    x_min = int(self.label_image_length * detection.Box.Left)
+                    y_min = int(self.label_image_length * detection.Box.Top)
+                    x_max = int(self.label_image_length * detection.Box.Right)
+                    y_max = int(self.label_image_length * detection.Box.Bottom)
+
+                    DrawRectangle(model_image, (x_min, y_min), (x_max, y_max), color)
+                    DrawText(model_image, label, (x_min+5, y_min+25), color)
+                    DrawText(model_image, '{:.2f}'.format(detection.Score), (x_min+5, y_min+50), color)
 
             SetLabelImageFromArray(self.Window.PrimaryLabel, primary_image)
             SetLabelImageFromArray(self.Window.ModelLabel, model_image)
